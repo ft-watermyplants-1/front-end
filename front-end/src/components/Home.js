471cc1691309d31797969879f961a031dd76e275
Fernando Martinez <fernando817mm@gmail.com>
2021-07-26 12:11:49 -0500

    mock

---
+++ front-end/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = (props) => {
+    return (
+        <div id = 'homePage'>
+            <h1>Water My Plants</h1>
+        </div>
+    )
+}
+
+export default Home;
